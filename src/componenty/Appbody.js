51d5c285bd1679801c56de6e94bb4c29d40e9858
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2022-06-22 09:32:12 +0200

    drugi commit

---
+++ src/componenty/Appbody.js
@@ -0,0 +1,13 @@
+import classes from "./Appbody.module.css";
+import GlownaTabela from "./GlownaTabela";
+
+const Appbody = () =>{
+    return(
+        <div className={classes.Appbody}>
+
+            <GlownaTabela/>
+        </div>
+    );
+}
+
+export default Appbody;
---
+++ src/componenty/Navigation.js
@@ -0,0 +1,12 @@
+import classes from './Navigation.module.css';
+
+const Navigation =() =>{
+    return (
+        <header className={classes.Appheader}>
+            <div className={classes.TextLewa}>
+            <p>LOGO</p>
+            </div>
+    </header>
+    );
+}
+export default Navigation;
